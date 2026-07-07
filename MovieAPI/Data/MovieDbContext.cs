3f9ff8bf1d57731ffd3237734cd4c9e9549b74b1
karthik M <karthikmanji4465@outlook.com>
2026-07-07 20:42:22 +0530

    - Added DB Context.

---
+++ MovieAPI/Data/MovieDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Model;
+
+namespace MovieAPI.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
