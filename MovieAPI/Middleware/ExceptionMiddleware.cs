7ee8467f2520f8b085ba5a8aad2140049a1a5d3f
karthik M <karthikmanji4465@outlook.com>
2026-07-11 20:49:00 +0530

    - Added Global exception middleware.

---
+++ MovieAPI/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            context.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            context.Response.ContentType =
+                "application/json";
+
+            var response = new
+            {
+                Success = false,
+
+                Message =
+                "An unexpected error occurred."
+            };
+
+            await context.Response.WriteAsync(
+                JsonSerializer.Serialize(response));
+        }
+    }
+}
